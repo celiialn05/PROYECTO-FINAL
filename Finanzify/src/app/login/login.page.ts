bf6d4fe2f29fb56bcf779b68df254eae3f7ece8d
Celia Lloret Nicolás <5558037@alu.murciaeduca.es>
2024-03-20 01:42:20 +0100

    RESOLUCION-PROBLEMA-ANDROID_20/03/2024

--- Finanzify/src/app/login/login.page.ts
+++ Finanzify/src/app/login/login.page.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule, DOCUMENT } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonContent, IonicModule } from '@ionic/angular';
 import { RouterLink, RouterModule } from '@angular/router';
 import { moon, sunnyOutline } from 'ionicons/icons';
 import { ThemeService } from '../services/theme.service';
+import { IonButton, IonButtons, IonFab, IonFabButton, IonFabList, IonFooter, IonHeader, IonItemDivider, IonTextarea } from '@ionic/angular/standalone';
 
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
-  standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, RouterModule,RouterLink]
+  standalone: true,                                                          //Para Android tendras que añadir estos imports 
+  imports: [IonicModule, CommonModule, FormsModule, RouterModule,RouterLink],//,IonHeader, IonFooter, IonButtons, IonButton, IonFabButton,IonItemDivider,IonTextarea,IonFabButton,IonFab,IonFabList
 })
 export class LoginPage implements OnInit {
   constructor(private themeService: ThemeService) {} 
