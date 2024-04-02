9ce94dd71fa7f4ab12bb2f105214f661b6600c10
Celia Lloret Nicolás <5558037@alu.murciaeduca.es>
2024-04-02 03:36:29 +0200

    PRUEBA CONEXION BBDD - 02/04/2024

--- Finanzify/src/app/dashboard/dashboard.page.ts
+++ Finanzify/src/app/dashboard/dashboard.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonButton, IonButtons, IonContent, IonFooter, IonHeader, IonTitle, IonToolbar,IonFabButton, IonItemDivider,IonTextarea,IonFab,IonFabList, IonCard, IonCardHeader, IonCardTitle, IonGrid, IonCol, IonRow } from '@ionic/angular/standalone';
 import {  IonicModule } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,10 +14,17 @@ import {  IonicModule } from '@ionic/angular';
   //imports: [IonicModule, CommonModule, FormsModule,IonGrid,IonCol,IonRow,IonHeader, IonFooter, IonButtons, IonButton, IonFabButton,IonItemDivider,IonTextarea,IonFabButton,IonFab,IonFabList,IonToolbar,IonTitle,IonContent,IonCard,IonCardHeader,IonCardTitle]
 })
 export class DashboardPage implements OnInit {
-
-  constructor() { }
+pruebas : any = [];
+  constructor(private http:HttpClient ) { }
 
   ngOnInit() {
   }
+
+  getPrueba(){
+    this.http.get('http://localhost/prueba.php').subscribe((response) => {
+    console.log(response);
+    this.pruebas = response;
+  });
+}
 
 }
