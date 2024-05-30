6a5bcac757202294c28b24ec1dfc6def58a5bd9e
Celia Lloret Nicolás <5558037@alu.murciaeduca.es>
2024-05-30 21:30:42 +0200

    CLINEADO CREAR CUENTA - CUMPLE

--- Finanzify/src/app/crear-cuenta/crear-cuenta.page.ts
+++ Finanzify/src/app/crear-cuenta/crear-cuenta.page.ts
@@ -19,25 +19,44 @@ export class CrearCuentaPage implements OnInit {
 
   selectedFile: File | undefined;
   imagenSeleccionada: string = '';
-  constructor(private http: HttpClient, private alertController: AlertController, private router: Router) { }
+  constructor(
+    private http: HttpClient,
+    private alertController: AlertController,
+    private router: Router) { }
 
   ngOnInit() { }
 
-  onFileSelected(event: any) {
+  //Función para seleccionar un archivo de imagen 
+  Ficheroseleccionado(event: any) {
     const file = event.target.files[0];
-    const reader = new FileReader();
 
-    reader.onload = () => {
-      this.imagenSeleccionada = reader.result as string;
-    };
+    if (file.type.match('image.*')) {
+      const reader = new FileReader();
 
-    reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.imagenSeleccionada = reader.result as string;
+      };
+
+      reader.readAsDataURL(file);
+    } else {
+      console.log('El archivo seleccionado no es una imagen.');
+      this.presentAlert('ERROR', 'Por favor, selecciona un archivo de imagen.');
+    }
   }
+  
   onImageSelect() {
     const fileInput = document.getElementById('imageInput') as HTMLInputElement;
     fileInput.click();
   }
-  async onRegister() {
+  validarDNI(dni: string): boolean {
+    const dniRegex = /^\d{8}[a-zA-Z]$/;
+    return dniRegex.test(dni);
+  }
+  validarEmail(email: string): boolean {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  }
+  async Registro() {
     const nombre = (document.getElementById('nombre') as HTMLInputElement).value;
     const apellidos = (document.getElementById('apellidos') as HTMLInputElement).value;
     const edad = (document.getElementById('edad') as HTMLInputElement).value;
@@ -46,11 +65,36 @@ export class CrearCuentaPage implements OnInit {
     const contrasena = (document.getElementById('contrasena') as HTMLInputElement).value;
     const repetirContrasena = (document.getElementById('repetirContrasena') as HTMLInputElement).value;
 
-    if (contrasena !== repetirContrasena) {
-      this.presentAlert('Error', 'Las contraseñas no coinciden.');
+
+    if (!this.selectedFile) {
+      this.presentAlert('Error', 'Por favor, seleccione una imagen.');
       return;
     }
-    
+
+    if (nombre === '' || apellidos === '' || edad === '' || email === '' || dni === '' || contrasena === '' || repetirContrasena === '') {
+      this.presentAlert('Error', 'No puede haber campos vacíos.');
+      return;
+    }
+    if (email === '' || !this.validarEmail(email)) {
+      this.presentAlert('Error', 'El correo electrónico no es válido.');
+      (document.getElementById('email') as HTMLInputElement).value = '';
+      return;
+    }
+
+    if (!this.validarDNI(dni)) {
+      this.presentAlert('Error', 'El DNI no es válido.');
+      (document.getElementById('dni') as HTMLInputElement).value = '';
+      return;
+    }
+
+    if (contrasena !== repetirContrasena) {
+      console.log('Las contraseñas no coinciden');
+      this.presentAlert('Error', 'Las contraseñas no coinciden.');
+      (document.getElementById('contrasena') as HTMLInputElement).value = '';
+      (document.getElementById('repetirContrasena') as HTMLInputElement).value = '';
+      return;
+    }
+
     const formData = new FormData();
     formData.append('nombre', nombre);
     formData.append('apellidos', apellidos);
@@ -58,9 +102,8 @@ export class CrearCuentaPage implements OnInit {
     formData.append('email', email);
     formData.append('dni', dni);
     formData.append('contrasena', contrasena);
-    if (this.selectedFile) {
-      formData.append('imagen', this.selectedFile, this.selectedFile.name);
-    }
+    formData.append('imagen', this.selectedFile, this.selectedFile.name);
+
 
     this.http.post('http://192.168.1.247/crearcuentas.php', formData)
       .subscribe(response => {
@@ -68,49 +111,7 @@ export class CrearCuentaPage implements OnInit {
         this.presentAlert('Éxito', 'Cuenta creada exitosamente.');
       }, error => {
         console.error(error);
-        this.presentAlert('Error', 'No se pudo crear la cuenta.');
-      });
-  }
-  crearCuenta() {
-    const nombre = (document.getElementById('nombre') as HTMLInputElement).value;
-    const apellidos = (document.getElementById('apellidos') as HTMLInputElement).value;
-    const edad = (document.getElementById('edad') as HTMLInputElement).value;
-    const email = (document.getElementById('email') as HTMLInputElement).value;
-    const dni = (document.getElementById('dni') as HTMLInputElement).value;
-    const contrasena = (document.getElementById('contrasena') as HTMLInputElement).value;
-    const repetirContrasena = (document.getElementById('repetirContrasena') as HTMLInputElement).value;
-
-
-    if (contrasena !== repetirContrasena) {
-      console.log('Las contraseñas no coinciden');
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append('dni', dni);
-    formData.append('contrasena', contrasena);
-    formData.append('nombre', nombre);
-    formData.append('apellido', apellidos);
-    formData.append('edad', edad);
-    formData.append('email', email);
-
-    // Obtener el archivo seleccionado
-    const fileInput = document.getElementById('imageInput') as HTMLInputElement;
-    const imagen = fileInput.files ? fileInput.files[0] : null;
-
-    if (imagen) {
-      formData.append('imagen', imagen);
-    }
-
-    this.http.post('http://192.168.1.247/crearcuentas.php', formData)
-      .subscribe((response) => {
-        console.log(response);
-        this.presentAlert('Éxito', 'Cuenta creada exitosamente.');
-        // Redireccionar al usuario a la página de inicio de sesión
-        this.router.navigate(['/login']);
-      }, (error) => {
-        console.error(error);
-        this.presentAlert('Error', 'No se pudo crear la cuenta.');
+        this.presentAlert('Error', 'Ya existe ese DNI en la base de datos.');
       });
   }
   async presentAlert(header: string, message: string) {
